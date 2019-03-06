c726f09a68763e6d40c3f780d57749a842a72c50
Joshua M Loran <joshua.loran@gmail.com>
2019-03-06 01:21:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -48,10 +48,10 @@ end
 def add_s(array)
   return_array = []
   array.each_with_index do |word, i|
-    if !i = 1
-      return_array << (word + "s")
-    else
+    if i = 1
       return_array << word
+    else
+      return_array << (word + "s")
     end
   end
   return_array
